c330eb750a5cd9b1204dafe568570ed2c569fe71
ruslantilovov2 <ruslantilovov2@gmail.com>
2022-12-08 12:07:51 +0500

    change

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@ const closeBtn = document.getElementById("close-btn");
 const openBtn = document.getElementById("open-btn");
 const siteHeader = document.getElementById("site-header");
 const siteHero = document.getElementById("hero");
+const plantsBar = document.getElementById("plantsBar");
 
 openBtn.addEventListener("click", () => {
     modal.style.display = "block";
     siteHeader.style.filter = "blur(1px)"
     siteHero.style.filter = "blur(5px)"
+    plantsBar.style.filter = "blur(5px)"
 })
 
 closeBtn.addEventListener("click", () => {
     modal.style.display = "none";
     siteHeader.style.filter = "blur(0px)"
     siteHero.style.filter = "blur(0px)"
+    plantsBar.style.filter = "blur(0px)"
 })
